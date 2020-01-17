c24bb2461f5381fbb08e88d53b9f9519f54d3e7a
sriramnemani <davulurisridevi@gmail.com>
2020-01-16 17:15:58 -0800

    Initial commit

---
+++ StudCurRegistration/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StudCurRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var studentCurInfo = new studInfo();
+            studentCurInfo.StudentId = 111;
+            studentCurInfo.StudFirstName = "Ram";
+            studentCurInfo.StudLastName = "Raj";
+            studentCurInfo.Studgender = "Male";
+            studentCurInfo.StudphNum = 4253194523;
+            studentCurInfo.StudAddress = "Bellevue";
+            studentCurInfo.StudEmailAdd = "ramraj@gmail.com"; 
+
+        }
+    }
+}
---
+++ StudCurRegistration/studInfo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudCurRegistration
+{
+    /// <summary>
+    /// This represnets a student information
+    /// to enroll the course and assignment scores.
+    /// </summary>
+    class studInfo
+    {
+        #region properties of Student inforation
+        /// <summary>
+        /// unique student email address
+        /// </summary>
+        public int StudentId {get; set;}
+        public string StudFirstName { get; set;}
+        public string StudLastName { get; set; }
+        public DateTime StudDoB { get; set; }
+        public string Studgender { get; set; }
+
+        public string StudEmailAdd { get; set; }
+        public string StudAddress { get; set; }
+        public long  StudphNum { get; set; }
+        public string courseID { get; set; }  
+        public long score { get; set; }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// enroll a course to student
+        /// </summary>
+        public void course(string courseID) 
+        {
+
+        }
+
+
+        #endregion
+    }
+
+}
